cc56d848c734b41d73edda0dc8470fc5206ac0a5
alfredothepasta <robertalexreed@gmail.com>
2019-11-12 21:35:34 -0700

    added more pages

--- javaScript/app.js
+++ javaScript/app.js
@@ -1,10 +1,1 @@
-document.addEventListener("scroll", function (e) {
-    const blackOverlay = document.getElementById("blackOverlay");
-    const overlayHeight = blackOverlay.scrollHeight;
-    const scrollHeight = window.scrollY;
-    const halfOverlayHeight = overlayHeight / 2;
-    if (scrollHeight > 0) {
-        blackOverlay.style.opacity = (scrollHeight /
-            (overlayHeight / 1.2)).toString();
-    }
-});
+// JS that is universal accross the site
--- javaScript/app.ts
+++ javaScript/app.ts
@@ -1,13 +1,1 @@
-document.addEventListener("scroll", function(e) {
-  const blackOverlay: HTMLElement = document.getElementById("blackOverlay");
-  const overlayHeight: number = blackOverlay.scrollHeight;
-  const scrollHeight = window.scrollY;
-  const halfOverlayHeight: number = overlayHeight / 2;
-
-  if (scrollHeight > 0) {
-    blackOverlay.style.opacity = (
-      scrollHeight /
-      (overlayHeight / 1.2)
-    ).toString();
-  }
-});
+// JS that is universal accross the site
