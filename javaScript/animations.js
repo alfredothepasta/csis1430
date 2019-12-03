3428c988f06ad91e00abf27a1c808741411fbc8c
Alex Reed <Alex.Reed@amgh.us>
2019-12-03 14:08:13 -0700

    Finished animation.html and js

--- javaScript/animations.js
+++ javaScript/animations.js
@@ -1,6 +1,5 @@
-let counter: number = 0;
-
-let videos: string[] = [
+let counter = 0;
+let videos = [
     '<iframe width="100%"  src="https://www.youtube.com/embed/u2aQIGcgtt4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
     '<iframe width="100%"  src="https://www.youtube.com/embed/GY-qAVR1HjE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
     '<iframe width="100%"  src="https://www.youtube.com/embed/b2mOopjYh8g" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
@@ -12,11 +11,10 @@ let videos: string[] = [
     '<iframe width="100%"  src="https://www.youtube.com/embed/vVcih1hommE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
     '<iframe width="100%"  src="https://www.youtube.com/embed/Ji9hVeA9v-I" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
 ];
-
-let videosDescriptionList: string[] = [
+let videosDescriptionList = [
     'My very first animation project, the idea was that we took a toy, plotted a course, and took photos for each frame. We didn\'t preplan the motion or speed, instead the animation was done "straight forward".',
     'My second animation project, whihc was all about "pose to pose" animation, where you draw the initial pose and the final pose, and plan the motion in between each.',
-    'We moved up in the world a bit, on towards animating two bouncing balls! The learning goal of this assignment was squash and stretch, note the way each ball elongates as it moves faster and squashes as it decelerates. The image is so dark because each ball was drawn on its own set of paper without a transparency. The two pictures were averaged together to make them both show up.', 
+    'We moved up in the world a bit, on towards animating two bouncing balls! The learning goal of this assignment was squash and stretch, note the way each ball elongates as it moves faster and squashes as it decelerates. The image is so dark because each ball was drawn on its own set of paper without a transparency. The two pictures were averaged together to make them both show up.',
     'No longer in the realm of 2D, this animation focused on the idea of follow through. Note the way the tail moves.',
     'Moving up in complexity now, this is the flour-sack animation. The idea is to help solidify the idea of adding personality to an object through animation.',
     'Moving forward a year, we\'re now at basic character animation. A simple walk cycle for a ball with legs.',
@@ -25,8 +23,7 @@ let videosDescriptionList: string[] = [
     'This was an animation I made for the Skills USA national competition 2014. My partner and I came in fourth place overall.',
     'This is my final production at SLCC. I made this with the help of Brandon Craghead, who did an excellent job on all of the modeling for the backgrounds and, notably, the piano. This is about as complete as it will ever get unless I decide to someday go back in and finish it. Knowing me though, I\'d have to redo half the animation to be happy. Enjoy!'
 ];
-
-let titles: string[] = [
+let titles = [
     'Straight Forward Animation',
     'Pose to Pose Animation',
     '2 Ball Bounce Animation',
@@ -37,51 +34,42 @@ let titles: string[] = [
     '3D Animation Compilation',
     'Skills USA Entry',
     'Final Project: Magnum Opus',
-]
-
-let animationTitle: HTMLElement = document.getElementById('animationTitle');
-let videoDescription: HTMLElement = document.getElementById('videoDescription');
-let videoSection: HTMLElement = document.getElementById('videoSection');
-
+];
+let animationTitle = document.getElementById('animationTitle');
+let videoDescription = document.getElementById('videoDescription');
+let videoSection = document.getElementById('videoSection');
 animationTitle.textContent = titles[0];
 videoDescription.textContent = videosDescriptionList[0];
 videoSection.innerHTML = videos[0];
-
-document.getElementById('navigation').addEventListener('click', function(e){
+document.getElementById('navigation').addEventListener('click', function (e) {
     e.preventDefault();
     let target = e.target;
-    let link: HTMLAnchorElement;
-
-    if(e.target instanceof HTMLAnchorElement){
+    let link;
+    if (e.target instanceof HTMLAnchorElement) {
         link = e.target;
         console.log(link);
-
-        if(link.id == 'next'){
+        if (link.id == 'next') {
             nextVid();
-        } else {
+        }
+        else {
             lastVid();
         }
-
-    } else {
-        
+    }
+    else {
     }
 });
-
-function nextVid(){
-    if(++counter == videos.length){
+function nextVid() {
+    if (++counter == videos.length) {
         counter = 0;
     }
-
     animationTitle.textContent = titles[counter];
     videoDescription.textContent = videosDescriptionList[counter];
     videoSection.innerHTML = videos[counter];
 }
-
-function lastVid(){
-    if(--counter < 0){
+function lastVid() {
+    if (--counter < 0) {
         counter = videos.length - 1;
     }
-
     animationTitle.textContent = titles[counter];
     videoDescription.textContent = videosDescriptionList[counter];
     videoSection.innerHTML = videos[counter];
